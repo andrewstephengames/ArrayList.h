4f608086b0498115f7e30d20a5d7264d68024bad
andrewstephengames <andrewstephengames999@gmail.com>
2024-02-14 12:52:17 +0200

    Add ArrayList.h

---
+++ ArrayList.h
@@ -0,0 +1,88 @@
+#ifndef ARRAYLIST_H_
+#define ARRAYLIST_H_
+
+#ifndef ALDEF
+#define ALDEF
+#endif // ALDEF
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+typedef union {
+     int Int;
+     size_t Size_t;
+     char Char;
+     float Float;
+     double Double;
+} Type;
+
+typedef struct {
+     Type *data;
+     size_t size;
+     size_t capacity;
+} Array;
+
+#define crash assert (a->data != NULL && "Buy more RAM lol")
+#define cast (void*)
+
+void ArrayInit (Array *a, size_t bytes); // Must use ArrayFree
+void ArrayAdd (Array *a, Type element);
+void ArrayClear (Array *a); // Must use ArrayFree
+Array ArrayClone (Array *a); // Must use ArrayFree
+void ArrayFree (Array *a);
+
+void ArrayInit (Array *a, size_t bytes) {
+     a->data = malloc (sizeof(Type) * bytes);
+     crash;
+     a->size = 0;
+     a->capacity = bytes;
+}
+
+void ArrayAdd (Array *a, Type element) {
+     if (a->size == a->capacity) {
+          a->capacity *= 2;
+          a->data = realloc (a->data, a->capacity * sizeof(Type));
+          crash;
+     }
+     a->data[a->size++] = element;
+}
+
+void ArrayClear (Array *a) {
+     free (a->data);
+     a->data = malloc (sizeof(Type) * a->size);
+     a->size = 0;
+}
+
+// FIXME: Float and Double elements not getting cloned
+Array ArrayClone (Array *a) {
+     Array b = {0};
+     b.data = malloc (sizeof(Type) * a->size);
+     for (size_t i = 0; i < a->size; ++i) {
+          if (a->data[i].Int != 0) {
+               b.data[b.size++].Int = a->data[i].Int;
+          }
+          if (a->data[i].Size_t != 0) {
+               b.data[b.size++].Size_t = a->data[i].Size_t;
+          }
+          if (a->data[i].Char != 0) {
+               b.data[b.size++].Char = a->data[i].Char;
+          }
+          if (a->data[i].Float != 0.0f) {
+               b.data[b.size++].Float = a->data[i].Float;
+          }
+          if (a->data[i].Double != 0.0) {
+               b.data[b.size++].Double = a->data[i].Double;
+          }
+     }
+     return b;
+}
+
+void ArrayFree (Array *a) {
+     free (a->data);
+     a->data = NULL;
+     a->size = 0;
+     a->capacity = 0;
+}
+
+#endif // ARRAYLIST_H_
